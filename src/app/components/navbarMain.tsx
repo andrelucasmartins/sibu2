f1be0988bece14230b687771cbe8215f87384ef3
André Ezequiel <andrelucasmartins@hotmail.com>
2023-09-13 20:19:51 -0300

    Feat: Use Terms Popup

--- src/app/components/navbarMain.tsx
+++ src/app/components/navbarMain.tsx
@@ -5,8 +5,8 @@ interface NavbarMainProps {}
 export const NavbarMain = (props: NavbarMainProps) => {
   return (
     <>
-      <section className="bg-red-900  text-white py-4">
-        <nav className="flex justify-between items-center gap-10 text-lg font-bold max-w-7xl mx-auto">
+      <section className="bg-red-900  text-white py-2">
+        <nav className="flex justify-between items-center gap-10 text-xl font-bold max-w-7xl mx-auto">
           <div>O melhor redutor de medidas!</div>
           <div>Frete Grátis Para Todo o Brasil!</div>
           <div>Satisfação Garantida</div>
