b07db73cdd88af9c375b9ca1d284078c72920310
André Ezequiel <andrelucasmartins@hotmail.com>
2023-09-16 09:59:43 -0300

    Feat: data modals

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,93 +1,12 @@
 "use client";
 
-import { v4 as uuidv4 } from "uuid";
-
 interface FooterProps {}
-
-const PRIVACY_POLICY = [
-  {
-    id: uuidv4(),
-    title: "TERMOS DA POLITICA DE PRIVACIDADE",
-    description:
-      "Todas as suas informações pessoais recolhidas, serão usadas para o ajudar a tornar a sua visita no nosso site o mais produtiva e agradável possível. A garantia da confidencialidade dos dados pessoais dos utilizadores do nosso site é importante para o sibutran2. Todas as informações pessoais relativas a membros, assinantes, clientes ou visitantes que usem o sibutran2 serão tratadas em concordância com a Lei da Proteção de Dados Pessoais de 26 de outubro de 1998 (Lei n.º 67/98). A informação pessoal recolhida pode incluir o seu nome, e-mail, número de telefone e/ou telemóvel, morada, data de nascimento e/ou outros. O uso do sibutran2 pressupõe a aceitação deste Acordo de privacidade. A equipe do sibutran2 reserva-se ao direito de alterar este acordo sem aviso prévio. Deste modo, recomendamos que consulte a nossa política de privacidade com regularidade de forma a estar sempre atualizado.",
-  },
-  {
-    id: uuidv4(),
-    title: "OS ANÚNCIOS",
-    description:
-      "Tal como outros websites, coletamos e utilizamos informação contida nos anúncios. A informação contida nos anúncios, inclui o seu endereço IP (Internet Protocol), o seu ISP (Internet Service Provider, como o Sapo, Clix, ou outro), o browser que utilizou ao visitar o nosso website (como o Internet Explorer ou o Firefox), o tempo da sua visita e que páginas visitou dentro do nosso website.",
-  },
-  {
-    id: uuidv4(),
-    title: "OS COOKIES E WEB BEACONS",
-    description:
-      "Utilizamos cookies para armazenar informação, tais como as suas preferências pessoas quando visita o nosso website. Isto poderá incluir um simples popup, ou uma ligação em vários serviços que providenciamos, tais como fóruns. Em adição também utilizamos publicidade de terceiros no nosso website para suportar os custos de manutenção. Alguns destes publicitários, poderão utilizar tecnologias como os cookies e/ou web beacons quando publicitam no nosso website, o que fará com que esses publicitários (como o Google através do Google AdSense) também recebam a sua informação pessoal, como o endereço IP, o seu ISP, o seu browser, etc. Esta função é geralmente utilizada para geotargeting (mostrar publicidade de Lisboa apenas aos leitores oriundos de Lisboa por ex.) ou apresentar publicidade direcionada a um tipo de utilizador (como mostrar publicidade de restaurante a um utilizador que visita sites de culinária regularmente, por ex.). Você detém o poder de desligar os seus cookies, nas opções do seu browser, ou efetuando alterações nas ferramentas de programas Anti-Vírus, como o Norton Internet Security. No entanto, isso poderá alterar a forma como interage com o nosso website, ou outros websites. Isso poderá afetar ou não permitir que faça logins em programas, sites ou fóruns da nossa e de outras redes.",
-  },
-  {
-    id: uuidv4(),
-    title: "LIGAÇÕES A SITES DE TERCEIROS",
-    description:
-      "O sibutran2 – composto natural para emagrecimento possui ligações para outros sites, os quais, a nosso ver, podem conter informações / ferramentas úteis para os nossos visitantes. A nossa política de privacidade não é aplicada a sites de terceiros, pelo que, caso visite outro site a partir do nosso deverá ler a politica de privacidade do mesmo. Não nos responsabilizamos pela política de privacidade ou conteúdo presente nesses mesmos sites.",
-  },
-];
-
-const TERMS = [
-  {
-    id: 1,
-    title: "TERMOS E CONDIÇÕES DE COMPRA E VENDA",
-    description:
-      "Considerando que a sibutran2 realiza venda de produtos e serviços pela internet; Considerando o interesse do Cliente na compra dos produtos oferecidos pelo sibutran2 (“Produtos”) em seus canais de venda; O presente contrato tem por finalidade estabelecer as condições gerais de uso e compra de produtos e serviços do cliente do site sibutran2.",
-  },
-  {
-    id: 2,
-    title: "I. Confidencialidade:",
-    description:
-      "É de responsabilidade sibutran2 a preservação da confidencialidade de todos os dados e informações fornecidos pelo Cliente no processo de compra. A segurança do site é auditada diariamente e garantida contra a ação de hackers, através do selo “Site Blindado”.",
-  },
-  {
-    id: 3,
-    title: "II. Serviço de Atendimento ao Cliente (SAC):",
-    description:
-      "O cliente dispõe desse serviço para sanar suas dúvidas, solucionar eventuais solicitações ou reclamações a respeito do seu pedido ou de qualquer conteúdo disponibilizado no site. O SAC poderá ser acionado por meio de telefone ou de formulário do site.",
-  },
-  {
-    id: 4,
-    title: "III. Política de entrega:",
-    description: `
-    o prazo para entrega dos Produtos é informado durante o procedimento de compra, contabilizado em dias úteis. As entregas dos Produtos são realizadas de segunda a sexta-feira, das 8h às 22h. Excepcionalmente, algumas entregas de Produtos podem ocorrer aos sábados, domingos e feriados.
-
-III.I – A conferência da adequação das dimensões do produto é de responsabilidade do Cliente, que deverá se assegurar de que estas estão de acordo com os limites espaciais dos elevadores, portas e corredores do local da entrega. Não será realizada a montagem ou desmontagem do produto, transporte pela escada e/ou portas e janelas, ou içamento das entregas.
-
-III.II – Serão realizadas até três tentativas de entrega no local informado, em dias alternados, com intervalo de até 48h entre uma entrega e outra. É indispensável que, no endereço solicitado, haja uma pessoa autorizada pelo comprador, maior de 18 anos, e portando documento de identificação para receber a mercadoria e assinar o protocolo de entrega. Se houver três tentativas de entrega sem sucesso, o pedido retornará para o Centro de Distribuição do sibutran2.
-
-III.III – Após a finalização do pedido não é possível alterar a forma de pagamento e/ou endereço de entrega, solicitar adiantamento ou, ainda, prioridade da entrega.
-
-III.IV – O prazo de entrega informado durante o procedimento de compra do Produto leva em consideração o estoque, a região, o processo de emissão da nota fiscal e o tempo de preparo do produto. A cada atualização no status de entrega do pedido, o sistema do sibutran2 envia, automaticamente, e-mails de alerta para o Cliente.
-
-III.V – O valor do frete da entrega é calculado com base no local de entrega, peso e dimensões do Produto.
-
-III.VI – O sibutran2 não autoriza a transportadora a: entrar no domicílio; entregar por meios alternativos (exemplo: içar produto por janela); realizar instalação ou manutenção de produtos; abrir a embalagem do produto; realizar entrega em endereço diferente do que consta no sibutran2; realizar entrega a menor de idade ou sem documento de identificação.
-
-III.VII – O sibutran2 não se responsabiliza pela retenção de mercadorias na SEFAZ quando esta se dever exclusivamente a pendências do cliente, sendo, portanto, necessário seu comparecimento no posto fiscal para que a mercadoria seja liberada, tendo em vista que nestes casos as informações referentes a liberações e pagamentos só são passadas aos interessados.
-    `,
-  },
-  {
-    id: 5,
-    title: "IV. Direito de arrependimento:",
-    description: `Ao Cliente será facultado o exercício do direito de arrependimento da compra, com a finalidade de devolução do Produto, hipótese na qual deverão ser observadas as seguintes condições: o prazo de desistência da compra do produto é de até 7 (sete) dias corridos, a contar da data do recebimento; em caso de devolução, o produto deverá ser devolvido à sibutran2 na embalagem original, acompanhado do DANFE (Documento Auxiliar da Nota Fiscal Eletrônica), do manual e de todos os seus acessórios.
-
-IV.I – O Cliente deverá solicitar a devolução através do Serviço de Atendimento ao Cliente (SAC) ou diretamente no Painel de Controle, no tópico “cancelar pedido”. As despesas decorrentes de coleta ou postagem do Produto serão custeadas pelo sibutran2.
-
-IV.II – Após a chegada do produto ao Centro de Distribuição, o sibutran2 verificará se as condições supra citadas foram atendidas. Em caso afirmativo, providenciará a restituição no valor total da compra.
-
-IV.III – Em compras com cartão de crédito a administradora do cartão será notificada e o estorno ocorrerá na fatura seguinte ou na posterior, de uma só vez, seja qual for o número de parcelas utilizado na compra. O prazo de ressarcimento e, ainda, a cobrança das parcelas remanescentes após o estorno integral do valor do Produto no cartão de crédito do Cliente realizado pelo sibutran2, é de responsabilidade da administradora do cartão. Na hipótese de cobrança de parcelas futuras pela administradora do cartão, o Cliente não será onerado, vez que o sibutran2, conforme mencionado acima, realiza o estorno do valor integral do Produto em uma única vez, sendo o crédito referente ao estorno concedido integralmente pela administradora do cartão na fatura de cobrança subsequente ao mês do cancelamento.
-
-IV.IV – Em compras pagas com boleto bancário ou débito em conta, a restituição será efetuada por meio de depósito bancário, em até 10 (dez) dias úteis, somente na conta corrente do(a) comprador(a), que deve ser individual. É necessário que o CPF do titular da conta corrente.`,
-  },
-];
 
 import Link from "next/link";
 import { FaExpeditedssl, FaWhatsapp } from "react-icons/fa6";
+import { ASSURANCE } from "../data/assurance";
+import { PRIVACY_POLICY } from "../data/privacy-policy";
+import { TERMS } from "../data/terms";
 import { Modal } from "./modal";
 
 export function Footer(props: FooterProps) {
@@ -147,9 +66,15 @@ export function Footer(props: FooterProps) {
             </h2>
             <ul className="text-gray-100 font-medium">
               <li className="mb-4">
-                <a href="#" className="hover:underline">
-                  Garantia
-                </a>
+                <Modal.UseTerms
+                  list={ASSURANCE}
+                  title="Garantia"
+                  id="assurance"
+                >
+                  <Modal.ButtonAction id="assurance">
+                    Garantia
+                  </Modal.ButtonAction>
+                </Modal.UseTerms>
               </li>
               <li className="mb-4">
                 <Modal.UseTerms
@@ -209,105 +134,12 @@ export function Footer(props: FooterProps) {
           </div>
         </div>
 
-        <div className="px-4 py-6 bg-gray-950 md:flex md:items-center md:justify-between mx-auto max-w-[1200px] ">
-          <span className="text-sm text-gray-200 dark:text-gray-300 sm:text-center">
+        <div className="px-4 py-6 bg-gray-950 md:flex md:items-center md:justify-center mx-auto max-w-7xl ">
+          <span className="text-sm text-gray-200 dark:text-gray-300 text-center">
             © {new Date().getFullYear()}{" "}
-            <a href="https://aedigi.com.br">AE Digi Solutions™</a>. All Rights
-            Reserved.
+            <a href="https://aedigi.com.br">AE Digi Solutions™</a>. Todos os
+            direitos reservados.
           </span>
-          <div className="flex mt-4 space-x-5 sm:justify-center md:mt-0">
-            <a
-              href="#"
-              className="text-gray-400 hover:text-gray-900 dark:hover:text-white"
-            >
-              <svg
-                className="w-4 h-4"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 8 19"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M6.135 3H8V0H6.135a4.147 4.147 0 0 0-4.142 4.142V6H0v3h2v9.938h3V9h2.021l.592-3H5V3.591A.6.6 0 0 1 5.592 3h.543Z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-              <span className="sr-only">Facebook page</span>
-            </a>
-            <a
-              href="#"
-              className="text-gray-400 hover:text-gray-900 dark:hover:text-white"
-            >
-              <svg
-                className="w-4 h-4"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 21 16"
-              >
-                <path d="M16.942 1.556a16.3 16.3 0 0 0-4.126-1.3 12.04 12.04 0 0 0-.529 1.1 15.175 15.175 0 0 0-4.573 0 11.585 11.585 0 0 0-.535-1.1 16.274 16.274 0 0 0-4.129 1.3A17.392 17.392 0 0 0 .182 13.218a15.785 15.785 0 0 0 4.963 2.521c.41-.564.773-1.16 1.084-1.785a10.63 10.63 0 0 1-1.706-.83c.143-.106.283-.217.418-.33a11.664 11.664 0 0 0 10.118 0c.137.113.277.224.418.33-.544.328-1.116.606-1.71.832a12.52 12.52 0 0 0 1.084 1.785 16.46 16.46 0 0 0 5.064-2.595 17.286 17.286 0 0 0-2.973-11.59ZM6.678 10.813a1.941 1.941 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.919 1.919 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Zm6.644 0a1.94 1.94 0 0 1-1.8-2.045 1.93 1.93 0 0 1 1.8-2.047 1.918 1.918 0 0 1 1.8 2.047 1.93 1.93 0 0 1-1.8 2.045Z" />
-              </svg>
-              <span className="sr-only">Discord community</span>
-            </a>
-            <a
-              href="#"
-              className="text-gray-400 hover:text-gray-900 dark:hover:text-white"
-            >
-              <svg
-                className="w-4 h-4"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 20 17"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M20 1.892a8.178 8.178 0 0 1-2.355.635 4.074 4.074 0 0 0 1.8-2.235 8.344 8.344 0 0 1-2.605.98A4.13 4.13 0 0 0 13.85 0a4.068 4.068 0 0 0-4.1 4.038 4 4 0 0 0 .105.919A11.705 11.705 0 0 1 1.4.734a4.006 4.006 0 0 0 1.268 5.392 4.165 4.165 0 0 1-1.859-.5v.05A4.057 4.057 0 0 0 4.1 9.635a4.19 4.19 0 0 1-1.856.07 4.108 4.108 0 0 0 3.831 2.807A8.36 8.36 0 0 1 0 14.184 11.732 11.732 0 0 0 6.291 16 11.502 11.502 0 0 0 17.964 4.5c0-.177 0-.35-.012-.523A8.143 8.143 0 0 0 20 1.892Z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-              <span className="sr-only">Twitter page</span>
-            </a>
-            <a
-              href="#"
-              className="text-gray-400 hover:text-gray-900 dark:hover:text-white"
-            >
-              <svg
-                className="w-4 h-4"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M10 .333A9.911 9.911 0 0 0 6.866 19.65c.5.092.678-.215.678-.477 0-.237-.01-1.017-.014-1.845-2.757.6-3.338-1.169-3.338-1.169a2.627 2.627 0 0 0-1.1-1.451c-.9-.615.07-.6.07-.6a2.084 2.084 0 0 1 1.518 1.021 2.11 2.11 0 0 0 2.884.823c.044-.503.268-.973.63-1.325-2.2-.25-4.516-1.1-4.516-4.9A3.832 3.832 0 0 1 4.7 7.068a3.56 3.56 0 0 1 .095-2.623s.832-.266 2.726 1.016a9.409 9.409 0 0 1 4.962 0c1.89-1.282 2.717-1.016 2.717-1.016.366.83.402 1.768.1 2.623a3.827 3.827 0 0 1 1.02 2.659c0 3.807-2.319 4.644-4.525 4.889a2.366 2.366 0 0 1 .673 1.834c0 1.326-.012 2.394-.012 2.72 0 .263.18.572.681.475A9.911 9.911 0 0 0 10 .333Z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-              <span className="sr-only">GitHub account</span>
-            </a>
-            <a
-              href="#"
-              className="text-gray-400 hover:text-gray-900 dark:hover:text-white"
-            >
-              <svg
-                className="w-4 h-4"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M10 0a10 10 0 1 0 10 10A10.009 10.009 0 0 0 10 0Zm6.613 4.614a8.523 8.523 0 0 1 1.93 5.32 20.094 20.094 0 0 0-5.949-.274c-.059-.149-.122-.292-.184-.441a23.879 23.879 0 0 0-.566-1.239 11.41 11.41 0 0 0 4.769-3.366ZM8 1.707a8.821 8.821 0 0 1 2-.238 8.5 8.5 0 0 1 5.664 2.152 9.608 9.608 0 0 1-4.476 3.087A45.758 45.758 0 0 0 8 1.707ZM1.642 8.262a8.57 8.57 0 0 1 4.73-5.981A53.998 53.998 0 0 1 9.54 7.222a32.078 32.078 0 0 1-7.9 1.04h.002Zm2.01 7.46a8.51 8.51 0 0 1-2.2-5.707v-.262a31.64 31.64 0 0 0 8.777-1.219c.243.477.477.964.692 1.449-.114.032-.227.067-.336.1a13.569 13.569 0 0 0-6.942 5.636l.009.003ZM10 18.556a8.508 8.508 0 0 1-5.243-1.8 11.717 11.717 0 0 1 6.7-5.332.509.509 0 0 1 .055-.02 35.65 35.65 0 0 1 1.819 6.476 8.476 8.476 0 0 1-3.331.676Zm4.772-1.462A37.232 37.232 0 0 0 13.113 11a12.513 12.513 0 0 1 5.321.364 8.56 8.56 0 0 1-3.66 5.73h-.002Z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-              <span className="sr-only">Dribbble account</span>
-            </a>
-          </div>
         </div>
       </div>
     </footer>
---
+++ src/app/data/assurance.ts
@@ -0,0 +1,49 @@
+import { v4 as uuidv4 } from "uuid";
+
+export const ASSURANCE = [
+  {
+    id: uuidv4(),
+    title: "TERMOS DA GARANTIA",
+    description: `Adquirido o produto via site www.sibutran2.com.br o consumidor estará automaticamente participando da campanha. Caso o consumidor não apresente resultados após 4 (quatro) semanas de uso, poderá requerer a devolução do valor pago exclusivamente pelo produto, não serão devolvidos valores referentes ao frete de envio e devolução.
+
+A fim de ter direito à devolução dos valores, o consumidor deverá seguir todas as orientações de utilização constantes nas embalagens do produto. O pedido deverá ser acompanhado de fotos de “antes” e “depois”, devendo aparecer nas fotos alguma comprovação da data de sua captura e o comprovante de compra do produto.
+
+Recebido o pedido de reembolso citado, o consumidor receberá um e-mail de confirmação com o endereço para onde o produto deverá ser remetido para análise. As análises visam estabelecer se houve contaminação do produto, falha no armazenamento, utilização irregular (utilização de quantidades maiores ou menores que a indicada) ou exposição a agentes que impeçam ou minimizem os efeitos pretendidos. Caso algum irregularidade seja constatada o reembolso não será efetuado.
+
+Os pedidos de reembolso deverão ser formalizados por e-mail em até 30 (Trinta) dias da compra do produto e seu recebimento pelo consumidor.
+
+Caso a devolução seja aprovada, o valor dos produtos será devolvido ao consumidor de acordo com a forma de pagamento utilizada no momento da compra. Salvo em casos em que não for possível, a devolução será efetivada na modalidade depósito bancário, na conta a ser fornecida pelo consumidor, em até 180 (cento e oitenta) dias, a contar da aprovação do reembolso.
+
+O sibutran2 não se responsabiliza por qualquer problema de força maior que possa interferir no processo de pedido de reembolso que impeça a sua concretização, como por exemplo greve dos serviços do correio, problemas com internet, funcionamento de e-mails, etc.`,
+  },
+  {
+    id: uuidv4(),
+    title: "TROCAS E DEVOLUÇÒES",
+    description: `Além do disposto nas regras referentes à garantia de resultados dos nossos produtos, que contam com regramento próprio referente a reembolso e devoluções, os produtos poderão ser devolvidos e as compras canceladas de acordo com a regras a seguir:
+
+O cancelamento das compras efetuadas em nosso site poderá ser feita em até 24 (vinte e quatro) horas depois da finalização da compra. Caso o pagamento da compra ainda não tenha sido aprovado no momento da compra, esta será cancelada de forma plena e nenhuma cobrança aparecerá na fatura do comprador.
+
+Caso o pagamento já tenha sido aprovado, e consequentemente debitado em sua fatura do cartão de crédito, será solicitado o estorno da compra. O estorno é feito pela operadora do cartão de crédito, assim, a forma de estorno, prazos e afins dependerão exclusivamente da operadora do cartão de crédito, não sendo de nossa responsabilidade o cumprimento de prazos ou solução de problemas.
+
+Caso o produto já tenha sido expedido, a compra não poderá ser cancelada, porém, o comprador poderá desistir da compra, mediante o envio do produto para o nosso depósito.
+
+Para efetuar a devolução de um produto, você deverá realizar sua solicitação em nossa central de atendimento em até 7 (sete) dias do seu recebimento. Caso do produto chegue ao nosso centro de distribuição sem a autorização da nossa central, o item será recusado.
+
+O comprador será o responsável pelos custos de devolução do produto e o produto deverá estar lacrado em sua embalagem original, que obrigatoriamente deverá estar intacta.
+
+Se o produto foi adquirido mediante boleto bancário, o reembolso será feito em conta corrente, que obrigatoriamente deverá ser de titularidade do responsável pelo pedido – cujo CPF deverá ser idêntico). O prazo do reembolso dependerá dos dados enviados e dos prazos bancários de praxe.
+
+A restituição do valor só será processada após a liberação da devolução pelo nosso departamento de controle de qualidade. O prazo para a liberação poderá ser de até 5 (cinco) dias uteis após o recebimento em nosso centro de distribuição.
+
+Se alguma das condições de devolução não for observada a liberação da restituição poderá ser negada. Nesse caso o produto será reenviado para o cliente e os custos de envio poderão ser cobrados.
+
+Avarias no produto recebido ou embalagem violada: Se no momento da entrega do produto houver avaria aparente ou a embalagem estiver violada, a entrega deverá ser recusada, o que gerará a devolução do produto ao nosso centro de distribuição, gerando o envio de um novo produto.
+
+Caso um terceiro autorizado (porteiro, empregado, recepção etc) receba a entrega do seu pedido ele deverá estar apto a checar as condições do produto recebido e recusar caso haja problemas.
+
+Caso a entrega seja feita por transportadora, no ato da entrega do pedido, informe ao transportador o motivo da recusa. Seu produto será devolvido ao nosso centro de distribuição. É de extrema importância você comunicar nossa central de atendimento imediatamente sobre esta recusa.
+
+Em todas as opções acima, o tempo de devolução do produto ao nosso centro de distribuição poderá variar, afetando o prazo para o processamento e envio de um novo produto.
+O prazo de recebimento do novo produto dependerá basicamente do CEP e da região de entrega/coleta do pedido e da forma de devolução escolhida.`,
+  },
+];
